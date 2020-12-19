41c33b1e4df4711868e4b8f8f61c458db8b74431
Shumatsu Mokuyobi <shumatsum@protonmail.ch>
2020-12-19 01:12:31 +0100

    Proper sample text and formatting

--- index.js
+++ index.js
@@ -1,46 +1,46 @@
 const tmi = require('tmi.js');
 
-let _active=false;
-const voter={};
-const characters={};
-const vote_moderators = ['Shumat_su']
+let _active = false;
+const voter = {};
+const characters = {};
+const vote_moderators = ['owner_name']
 const client = new tmi.Client({
     connection: {
         secure: true,
         reconnect: true
     },
-    channels: [ 'shumat_su' ]
+    channels: ['channel_name']
 });
 
 client.connect();
 
 client.on('message', (channel, tags, message, self) => {
     console.log(`${tags['display-name']}: ${message}`);
-    if(message.startsWith('+startvote') && vote_moderators.includes(tags['display-name'])) _active=true;
-    if(message.startsWith('+endvote') && vote_moderators.includes(tags['display-name'])) _active=false;
-    if(message.startsWith('+results') && vote_moderators.includes(tags['display-name'])) {
+    if (message.startsWith('+startvote') && vote_moderators.includes(tags['display-name'])) _active = true;
+    if (message.startsWith('+endvote') && vote_moderators.includes(tags['display-name'])) _active = false;
+    if (message.startsWith('+results') && vote_moderators.includes(tags['display-name'])) {
         let _characters = [];
-        for (let character in characters){
-            _characters.push([character,characters[character]])
+        for (let character in characters) {
+            _characters.push([character, characters[character]])
         }
-        console.log(_characters.sort(function(a,b){
+        console.log(_characters.sort(function (a, b) {
             return b[1] - a[1];
         }));
         return;
     }
 
-    if(!_active) return;
-    if(self || !message.startsWith('+')) return;
+    if (!_active) return;
+    if (self || !message.startsWith('+')) return;
 
     const args = message.slice(1).split(' ');
     const command = args.shift().toLowerCase();
 
-    if(command === 'vote') {
+    if (command === 'vote') {
         console.log(`${tags['display-name']} voted for ${args.join(' ')}`)
-        if( voter[tags['display-name']] !== true){
+        if (voter[tags['display-name']] !== true) {
             // voter[tags['display-name']] = true;
             console.log(`One vote for ${args}!`)
-            if (!characters[args]) characters[args]=0;
+            if (!characters[args]) characters[args] = 0;
             characters[args]++;
         } else {
             console.log(`Repeat voter!`);
